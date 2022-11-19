bd386444c52e90f08b74f91a1d8add0d7d9ff9bc
lakhoune <ben.lakhoune@rwth-aachen.de>
2022-11-19 17:51:36 +0100

    rename pm4py test file

--- pyinsights/log-skeleton/pm4py.py
+++ pyinsights/log-skeleton/pm4py.py
@@ -1,9 +1,8 @@
 import pm4py
 import os
 if __name__ == "__main__":
-    log = pm4py.read_xes(os.path.join("example.xes"))
+    log = pm4py.read_xes(os.path.join("../event-logs/example.xes"))
     from copy import deepcopy
     filtered_log = pm4py.filter_variants_top_k(log, 3)
     print("Filtered log:")
     print(filtered_log)
-
