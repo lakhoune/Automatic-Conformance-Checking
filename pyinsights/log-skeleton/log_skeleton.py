1853bf3fbad099ef132a5cdb0bf37eb7e9faa337
lakhoune <ben.lakhoune@rwth-aachen.de>
2022-11-19 19:38:32 +0100

    setup code structure for log skeleton

--- pyinsights/log-skeleton/log_skeleton.py
+++ pyinsights/log-skeleton/log_skeleton.py
@@ -28,9 +28,130 @@ class LogSkeleton:
         act_col = self.connector.activity_col()
         timestamp = self.connector.timestamp()
 
-    def log_skeleton(self, noise_treshold):
+    def get_log_skeleton(self, noise_threshold):
         """
         Returns the log skeleton of the data model.
-        :param noise_treshold: int
+        :param noise_threshold: int
         :return: pandas.DataFrame
         """
+        log_skeleton = None
+        # Get the extended log
+        extended_log = self._extend_log()
+
+        equivalence, always_after, always_before, never_together, directly_follows = self._get_relations(
+            extended_log, noise_threshold)
+
+        directly_follows_counter = None
+        sum_counter = None
+        min_counter = None
+        max_counter = None
+        log_skeleton = (equivalence, always_after, always_before, never_together,
+                        directly_follows, directly_follows_counter, sum_counter, min_counter, max_counter)
+
+        return log_skeleton
+
+    def _extend_log(self):
+        """
+        Extends the log by adding an artificial start and end event to each trace.
+        :return: pandas.DataFrame
+        """
+        extended_log = None
+        # Get the first and last event of each trace
+
+        return extended_log
+
+    def _get_relations(self, extended_log, noise_threshold):
+        """
+        Returns the relations of the log skeleton.
+        :param extended_log: pandas.DataFrame
+        :param noise_threshold: int
+        :return: pandas.DataFrame
+        """
+        equivalence = self._get_equivalence(extended_log, noise_threshold)
+        always_after = self._get_always_after(extended_log, noise_threshold)
+        always_before = self._get_always_before(extended_log, noise_threshold)
+        never_together = self._get_never_together(
+            extended_log, noise_threshold)
+        directly_follows = self._get_directly_follows(
+            extended_log, noise_threshold)
+        # Get the relations
+
+        return equivalence, always_after, always_before, never_together, directly_follows
+
+    def _get_equivalence(self, extended_log, noise_threshold):
+        """
+        Returns the equivalence relation of the log skeleton.
+        :param extended_log: pandas.DataFrame
+        :param noise_threshold: int
+        :return: pandas.DataFrame
+        """
+        equivalence = None
+        # Get the equivalence relation
+
+        return equivalence
+
+    def _get_always_after(self, extended_log, noise_threshold):
+        """
+        Returns the always after relation of the log skeleton.
+        :param extended_log: pandas.DataFrame
+        :param noise_threshold: int
+        :return: pandas.DataFrame
+        """
+        always_after = None
+        # Get the always after relation
+
+        return always_after
+
+    def _get_always_before(self, extended_log, noise_threshold):
+        """
+        Returns the always before relation of the log skeleton.
+        :param extended_log: pandas.DataFrame
+        :param noise_threshold: int
+        :return: pandas.DataFrame
+        """
+        always_before = None
+        # Get the always before relation
+
+        return always_before
+
+    def _get_never_together(self, extended_log, noise_threshold):
+        """
+        Returns the never together relation of the log skeleton.
+        :param extended_log: pandas.DataFrame
+        :param noise_threshold: int
+        :return: pandas.DataFrame
+        """
+        never_together = None
+        # Get the never together relation
+
+        return never_together
+
+    def _get_directly_follows(self, extended_log, noise_threshold):
+        """
+        Returns the directly follows relation of the log skeleton.
+        :param extended_log: pandas.DataFrame
+        :param noise_threshold: int
+        :return: pandas.DataFrame
+        """
+        directly_follows = None
+        # Get the directly follows relation
+
+        return directly_follows
+
+
+def log_subsumes_log(log1, log2):
+    """
+    Checks if log1 subsumes log2.
+    :return: bool
+    """
+    # Check if log1 subsumes log2
+    return False
+
+
+def log_subsumes_trace(log, trace):
+    """
+    Checks if log subsumes trace.
+    :return: bool
+    """
+    # Check if log subsumes trace
+    return False
