a3668ce02bfd88b882c4add34c566b00d048ad8c
lakhoune <ben.lakhoune@rwth-aachen.de>
2022-11-25 13:53:45 +0100

    use default credentials for now

--- pyinsights/log_skeleton/main.py
+++ pyinsights/log_skeleton/main.py
@@ -8,22 +8,19 @@ dirname = os.path.dirname(__file__)
 load_dotenv(os.path.join(dirname, "../../.env"))
 
 if __name__ == "__main__":
-    celonis_url = os.getenv(
-        "CELONIS_URL") or "https://academic-rastoder-erdzan-rwth-aachen-de.eu-2.celonis.cloud/"
-    api_token = os.getenv(
-        "API_TOKEN") or "MDVkYWJkOGMtMDQ1OC00Mjc2LTk4ZjEtYzFkYTM5ZTliN2Q2OjA5WnZvUGtqUkNEK1JjUE9lVzMrckNUUm8vbnJ0WXBodmNnK0dCNTJDeDVi"
-    key_type = os.getenv(
-        "KEY_TYPE") or "APP_KEY"
+    celonis_url = "https://academic-rastoder-erdzan-rwth-aachen-de.eu-2.celonis.cloud/"
+    api_token = "MDVkYWJkOGMtMDQ1OC00Mjc2LTk4ZjEtYzFkYTM5ZTliN2Q2OjA5WnZvUGtqUkNEK1JjUE9lVzMrckNUUm8vbnJ0WXBodmNnK0dCNTJDeDVi"
+    key_type = "APP_KEY"
     connector = Connector(api_token=api_token,
                           url=celonis_url, key_type=key_type)
     # choose data model
-    print("Available datamodels:")
-    print(connector.celonis.datamodels)
+    # print("Available datamodels:")
+    # print(connector.celonis.datamodels)
 
-    print("Input id of datamodel:")
-    id = input()
-    connector.set_paramters(model_id=id)
+    # print("Input id of datamodel:")
+    # id = input()
+    # connector.set_paramters(model_id=id)
 
     log_skeleton = LogSkeleton(connector=connector)
 
-    log_skeleton.get_equivalence()
+    log_skeleton._extend_log()
