772142c02056c3f6afb6f7fa3834495372ab3285
infer742 <christian.fiedler1@rwth-aachen.de>
2022-12-16 22:42:15 +0100

    same

--- pyinsights/organisational_profiling/compliance.py
+++ pyinsights/organisational_profiling/compliance.py
@@ -2,17 +2,18 @@ from pyinsights import Connector
 from pycelonis.celonis_api.pql.pql import PQL, PQLColumn, PQLFilter
 
 
-def segregation_of_duties(connector, resource_column):
+def segregation_of_duties(connector):
     """
     computes violations of the four-eyes principle
     :param connector: pycelonis.Connector
-    :param resource_column: name of resource column in event log
     :return: pandas.core.Dataframe
     """
+
+    resource_column = connector.resource_column()
     act_source = f"""SOURCE("{connector.activity_table()}"."{connector.activity_col()}") """
     act_target = f"""TARGET("{connector.activity_table()}"."{connector.activity_col()}") """
     case_id = f"""SOURCE("{connector.activity_table()}"."{connector.case_col()}") """
-    resource = f"""SOURCE("{connector.activity_table()}"."{resource_column}") """
+    resource = f"""SOURCE("{connector.activity_table()}"."{connector.resource_column()}") """
 
     filter = f""" FILTER SOURCE("{connector.activity_table()}"."{resource_column}") = TARGET("{connector.activity_table()}"."{resource_column}")   """
 
@@ -26,4 +27,3 @@ def segregation_of_duties(connector, resource_column):
     df = connector.datamodel.get_data_frame(query)
 
     return df
-
