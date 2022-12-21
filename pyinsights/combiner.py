761f5dafc46390eaf62dffbabf74a7a6bc132f52
lakhoune <a.b.lakhoune@gmail.com>
2022-12-21 16:57:31 +0100

    comments

--- pyinsights/combiner.py
+++ pyinsights/combiner.py
@@ -53,45 +53,47 @@ class Combiner:
 
         result = pd.DataFrame(columns=[case_col, act_col, timestamp])
         # prepare dataframes
-        for df in deviations_df:
-
+        for method, df in deviations_df.items():
+            if df.empty:
+                continue
             if how == "union":
+                conformance_cols = [
+                    method + " conforms" for method in deviations_df.keys()]  # these columns are used to check if the entry conforms to the given method
                 if "source" in list(df.columns.values):
-                    df.loc[:, act_col] = df.apply(axis=1, func=lambda x: f""" {x["source"]} -> {x["target"]}""")
+                    df.loc[:, act_col] = df.apply(
+                        axis=1, func=lambda x: f""" {x["source"]} -> {x["target"]}""")  # transform source and target to one column
 
-                columns_to_drop = list(df.columns.values)
-                columns_to_drop.remove(case_col)
-                columns_to_drop.remove(act_col)
-                columns_to_drop.remove(timestamp)
+                # final columns of dataframe that is returned
+                final_columns = [case_col, act_col,
+                                 timestamp] + conformance_cols
+                columns_to_drop = [col for col in list(
+                    df.columns.values) if col not in final_columns]  # drop all columns that are not needed
             else:
-                if "source" not in list(df.columns.values):
+                if "source" not in list(df.columns.values) and act_col in list(df.columns.values):
                     df.loc[:, "source"] = df.loc[:, act_col]
                     df.loc[:, "target"] = "(atomic activity)"
-                columns_to_drop = list(df.columns.values)
-                columns_to_drop.remove(case_col)
-                columns_to_drop.remove("source")
-                columns_to_drop.remove("target")
-                columns_to_drop.remove(timestamp)
+                columns_to_drop = [col for col in df.columns.values if col not in [
+                    case_col, timestamp, act_col]]
 
             df.drop(columns_to_drop, axis=1, inplace=True)
 
+        # combine dataframes
         if how == "union":
-            for df in deviations_df:
-                result = pd.concat([result, df], join="outer", ignore_index=True)
-
+            for method, df in deviations_df.items():
+                result = pd.concat(
+                    [result, df], join="outer", ignore_index=True)  # outer join
+            result[conformance_cols] = result[conformance_cols].fillna(
+                value=True)  # fills empty cells with True (conforms)
         elif how == "intersection":
-
-            result = deviations_df[0]
-
-            for i in range(1,len(deviations_df)):
-                df = deviations_df[i]
-                result[[case_col, timestamp]] = result[result[[case_col, timestamp]].isin(df[[case_col, timestamp]])].dropna(how='all')[[case_col, timestamp]]
+            result = list(deviations_df.values())[0]
+            for i in range(1, len(deviations_df.values())):
+                df = list(deviations_df.values())[i]
+                temp_cols = [case_col, timestamp] if timestamp in df.columns else [
+                    case_col]
+                result[temp_cols] = result[result[temp_cols].isin(
+                    df[temp_cols])].dropna(how='all')[temp_cols]  # intersection
                 result.dropna(inplace=True)
 
         result.sort_values(by=[case_col, timestamp], inplace=True)
 
         return result
-
-
-
-
