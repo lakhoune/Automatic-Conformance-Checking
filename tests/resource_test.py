8f4460134f8a6f48b410f5b43bcd8f5234031919
infer742 <christian.fiedler1@rwth-aachen.de>
2023-01-08 17:13:31 +0100

    amended resource test to changes

--- tests/resource_test.py
+++ tests/resource_test.py
@@ -15,26 +15,28 @@ class ResourceTester(unittest.TestCase):
         :return:
         """
         # define connector and connect to celonis
-        connector = Connector(api_token=self.api_token, url=self.celonis_url, key_type="USER_KEY")
+        connector = Connector(api_token=self.api_token,
+                              url=self.celonis_url, key_type="USER_KEY")
         connector.set_parameters(
-            model_id="32b0abb8-bbcf-4700-8123-d11443e57bdd", end_timestamp='end_time')
+            model_id="32b0abb8-bbcf-4700-8123-d11443e57bdd", end_timestamp='end_time', resource_column="Resource")
         # compute resource profile
-        profiler = ResourceProfiler(connector=connector, resource_column="Resource")
-        resource_profile = profiler.resource_profile(time_unit="HOURS", reference_unit="DAY")
+        profiler = ResourceProfiler(connector=connector)
+        resource_profile = profiler.resource_profile(
+            time_unit="HOURS", reference_unit="DAY")
 
         expected_profile = pd.DataFrame(
             {
-                 connector.case_col(): [1, 1, 1, 2, 2, 2, 3, 4, 5, 6, 7, 8, 9],
-                 connector.activity_col(): ["a", "b", "c", "a", "a", "c", "a", "a", "a", "a", "a", "a", "a"],
-                 "resource": ["Pete", "Pete", "Pete", "Pete", "Pete", "Pete", "Pete", "Pete", "Pete", "Pete",
-                              "Pete", "Pete", "Pete"],
-                 "# this HOURS": [1, 1, 1, 1, 1, 1, 7, 7, 7, 7, 7, 7, 7],
-                 "# this DAY": [1, 1, 1, 2, 2, 1, 7, 7, 7, 7, 7, 7, 7]
+                connector.case_col(): [1, 1, 1, 2, 2, 2, 3, 4, 5, 6, 7, 8, 9],
+                connector.activity_col(): ["a", "b", "c", "a", "a", "c", "a", "a", "a", "a", "a", "a", "a"],
+                "Resource": ["Pete", "Pete", "Pete", "Pete", "Pete", "Pete", "Pete", "Pete", "Pete", "Pete",
+                             "Pete", "Pete", "Pete"],
+                "# this HOURS": [1, 1, 1, 1, 1, 1, 7, 7, 7, 7, 7, 7, 7],
+                "# this DAY": [1, 1, 1, 2, 2, 1, 7, 7, 7, 7, 7, 7, 7]
 
             })
 
         # assert that profile equals expected output
-        self.assertTrue(resource_profile[[connector.case_col(), connector.activity_col(), "resource",
+        self.assertTrue(resource_profile[[connector.case_col(), connector.activity_col(), "Resource",
                                           "# this HOURS", "# this DAY"]].equals(expected_profile))
 
     def test_cases_with_batches(self):
@@ -43,19 +45,19 @@ class ResourceTester(unittest.TestCase):
         :return:
         """
         # define connector and connect to celonis
-        connector = Connector(api_token=self.api_token, url=self.celonis_url, key_type="USER_KEY")
+        connector = Connector(api_token=self.api_token,
+                              url=self.celonis_url, key_type="USER_KEY")
         connector.set_parameters(
-            model_id="32b0abb8-bbcf-4700-8123-d11443e57bdd", end_timestamp='end_time')
+            model_id="32b0abb8-bbcf-4700-8123-d11443e57bdd", end_timestamp='end_time', resource_column="Resource")
         # compute cases with batches
-        profiler = ResourceProfiler(connector=connector, resource_column="Resource")
+        profiler = ResourceProfiler(connector=connector)
         cases_df = profiler.cases_with_batches(time_unit="HOURS", reference_unit="DAY", min_batch_size=2,
                                                grouped_by_batches=True, batch_types=True)
-
         expected_cases = pd.DataFrame(
             {
                 connector.case_col(): [3, 4, 5, 6, 7, 8, 9],
                 connector.activity_col(): ["a", "a", "a", "a", "a", "a", "a"],
-                "resource": ["Pete", "Pete", "Pete", "Pete", "Pete", "Pete", "Pete"],
+                "Resource": ["Pete", "Pete", "Pete", "Pete", "Pete", "Pete", "Pete"],
                 "# this HOURS": [7, 7, 7, 7, 7, 7, 7],
                 "# this DAY": [7, 7, 7, 7, 7, 7, 7],
                 "batch type": ["simultaneous", "simultaneous", "simultaneous", "simultaneous",
@@ -64,7 +66,7 @@ class ResourceTester(unittest.TestCase):
             })
 
         # assert that cases with batches equal expected output
-        self.assertTrue(cases_df[[connector.case_col(), connector.activity_col(), "resource",
+        self.assertTrue(cases_df[[connector.case_col(), connector.activity_col(), "Resource",
                                   "# this HOURS", "# this DAY", "batch type"]].equals(expected_cases))
 
 
